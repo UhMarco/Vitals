8d63e6cdb783eb100b9800ebca9e19ed8a9c74a1
Marco <marco.wyatt@rocketmail.com>
2023-06-08 00:29:00 +0100

    Gamemode command

--- src/main/java/com/marco/vitals/Vitals.java
+++ src/main/java/com/marco/vitals/Vitals.java
@@ -1,17 +1,12 @@
 package com.marco.vitals;
 
+import com.marco.vitals.commands.GamemodeCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Vitals extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+        getCommand("gamemode").setExecutor(new GamemodeCommand());
     }
 }
--- src/main/java/com/marco/vitals/commands/GamemodeCommand.java
+++ src/main/java/com/marco/vitals/commands/GamemodeCommand.java
@@ -1,6 +1,8 @@
 package com.marco.vitals.commands;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -10,27 +12,50 @@ public class GamemodeCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player player) {
-            Player target = (args.length >= 1) ? Bukkit.getPlayer(args[0]) : player;
-
-            if (!player.hasPermission("vitals.gamemode") || (player != target && !player.hasPermission("vitals.gamemode.others"))) {
-                player.sendMessage("Insufficient permissions.");
+            if (!player.hasPermission("vitals.gamemode")) {
+                player.sendMessage(ChatColor.RED + "Insufficient permissions.");
                 return true;
             }
 
-            switch (label) {
-                case "gamemode":
-                case "gm":
-                    break;
-                case "gmc":
-                    break;
-                case "gms":
-                    break;
-                case "gmsp":
-                    break;
+            int index = label.equals("gamemode") || label.equals(("gm")) ? 1 : 0;
+            Player target = (args.length > index) ? Bukkit.getPlayer(args[index]) : player;
+
+            if (target == null) {
+                player.sendMessage(ChatColor.RED + "Player " + args[index] + " not found.");
+                return true;
+            } else if (player != target && !player.hasPermission("vitals.gamemode.other")) {
+                player.sendMessage(ChatColor.RED + "Insufficient permissions.");
+                return true;
+            }
+
+            // TODO: Send message to admins that someone has updated someone's gamemode.
+
+            if (index == 1) {
+                if (args.length == 0) {
+                    player.sendMessage(ChatColor.GRAY + "Usage: /gamemode <gamemode> [target]");
+                    return true;
+                }
+                String gm = args[0].toLowerCase();
+                switch (gm) {
+                    case "creative", "c" -> target.setGameMode(GameMode.CREATIVE);
+                    case "survival", "s" -> target.setGameMode(GameMode.SURVIVAL);
+                    case "spectator", "sp" -> target.setGameMode(GameMode.SPECTATOR);
+                    case "adventure", "a" -> target.setGameMode(GameMode.ADVENTURE);
+                    default -> player.sendMessage(ChatColor.GRAY + "Usage: /gamemode <gamemode> [target]");
+                }
+            } else {
+                switch (label) {
+                    case "gmc" -> target.setGameMode(GameMode.CREATIVE);
+                    case "gms" -> target.setGameMode(GameMode.SURVIVAL);
+                    case "gmsp" -> target.setGameMode(GameMode.SPECTATOR);
+                    case "gma" -> target.setGameMode(GameMode.ADVENTURE);
+                }
             }
 
             return true;
         }
+
+        sender.sendMessage(ChatColor.RED + "Only players can run this command.");
         return false;
     }
 }
